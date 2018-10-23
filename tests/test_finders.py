0e224586bf325ae06c0f31af1d28fd5fab4445d2
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-23 14:29:36 -0600

    update test to reflect new behavior

--- tests/test_finders.py
+++ tests/test_finders.py
@@ -10,9 +10,8 @@ def test_WatershedFinder():
     # check that we get GeoJSON for valid requests
     f = finders.WatershedFinder()
     r = f.query(state='NY', lon=-74.524, lat=43.939)
-    rjson = r.json()
-    rjson_keys = rjson.keys()
-    assert 'workspaceID' in rjson_keys
-    assert 'featurecollection' in rjson_keys
-    assert 'parameters' in rjson_keys
-    assert 'messages' in rjson_keys
+    r_keys = r.keys()
+    assert 'workspaceID' in r_keys
+    assert 'featurecollection' in r_keys
+    assert 'parameters' in r_keys
+    assert 'messages' in r_keys
