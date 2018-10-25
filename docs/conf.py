6d7b10a94c93487c72771b4214b76aa208779294
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-25 14:37:49 -0600

    maybe the ordering of imports is important here...

--- docs/conf.py
+++ docs/conf.py
@@ -18,10 +18,11 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
-import streamstats
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+import streamstats
 
 # -- General configuration ---------------------------------------------
 
