fe3b019654e38a8e5f62cdc0519aa7da5915a55f
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-25 22:05:50 -0600

    Use geopy to find state, given lat/lon (#14)
    * Use geopy to find state, given lat/lon
    
    * add deps to setup.py and tox.ini
    
    * Update docs to reflect streamlined api

--- docs/usage.rst
+++ docs/usage.rst
@@ -8,11 +8,11 @@ Finding the watershed that contains a point
 Given a spatial point in the U.S. defined by a lat/lon location and a state,
 the `WatershedFinder` class can be used to find the watershed that contains
 that point.
-First, we define a watershed finder object that can be queried with a two
-character state code, and longitude/latitude values.
+First, we define a watershed finder object that can be queried with 
+longitude/latitude values.
 The `finders.find_watershed` function returns the JSON response from the `USGS
 StreamStats API <https://streamstats.usgs.gov/docs/streamstatsservices/>`_.
 
     from streamstats import finders
 
-    res = finders.find_watershed(state='NY', lon=-74.524, lat=43.939)
+    res = finders.find_watershed(lon=-74.524, lat=43.939)
--- streamstats/finders.py
+++ streamstats/finders.py
@@ -2,14 +2,13 @@
 """Functionality for finding watershed information for specific locations."""
 
 import requests
+import geopy
 import streamstats
 
 
-def find_watershed(state, lon, lat):
+def find_watershed(lon, lat):
     """Find the watershed that contains a point
 
-    :param state: Two character state code, e.g. "CA" for California.
-    :type state: string
     :param lon: Longitude of point in decimal degrees.
     :type lon: float
     :param lat: Latitude of point in decimal degrees.
@@ -18,7 +17,7 @@ def find_watershed(state, lon, lat):
     """
     baseurl = "".join([streamstats.BASE_URL, "watershed.geojson"])
     payload = {
-        'rcode': state,
+        'rcode': find_state(lon, lat),
         'xlocation': lon,
         'ylocation': lat,
         'crs': 4326,
@@ -30,3 +29,74 @@ def find_watershed(state, lon, lat):
     response = requests.get(baseurl, params=payload)
     response.raise_for_status()  # raises errors early
     return response.json()
+
+
+def find_state(lon, lat):
+    """Find the U.S. state that contains a point
+
+    :param lon: Longitude of point in decimal degrees
+    :type lon: float
+    :param lat: Latitude of point in decimal degrees
+    :type lat: float
+    :rtype: string of the state code (e.g., CO for Colorado)
+    """
+    state_locator = geopy.geocoders.Nominatim(user_agent='streamstats')
+    location_info = state_locator.reverse(", ".join([str(lat), str(lon)]))
+    address = location_info.raw['address']
+    assert address['country'] == 'USA', 'Point must be in U.S.'
+    state_code = US_STATE_ABBREV[address['state']]
+    return state_code
+
+
+US_STATE_ABBREV = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
