c90d66d898b8e552a2ea838955e96ab12c19e6c9
Max Joseph <maxwellbjoseph@gmail.com>
2018-10-26 13:09:13 -0600

    uncomment test: codecov appears to be working!

--- tests/test_finders.py
+++ tests/test_finders.py
@@ -7,14 +7,14 @@ import pytest
 from streamstats import finders
 
 
-# def test_find_watershed():
-#     """Verify that the JSON response contains expected keys."""
-#     resp = finders.find_watershed(lon=-74.524, lat=43.939)
-#     resp_keys = resp.keys()
-#     assert 'workspaceID' in resp_keys
-#     assert 'featurecollection' in resp_keys
-#     assert 'parameters' in resp_keys
-#     assert 'messages' in resp_keys
+def test_find_watershed():
+    """Verify that the JSON response contains expected keys."""
+    resp = finders.find_watershed(lon=-74.524, lat=43.939)
+    resp_keys = resp.keys()
+    assert 'workspaceID' in resp_keys
+    assert 'featurecollection' in resp_keys
+    assert 'parameters' in resp_keys
+    assert 'messages' in resp_keys
 
 
 def test_find_state():
