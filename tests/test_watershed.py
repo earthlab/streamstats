a4986507a44b9a285d12252d7c9e614644a05b98
Scott Eilerman <scott.eilerman@jupiterintel.com>
2018-11-01 13:25:11 -0600

    add tests for get_geojson and get_boundary

--- tests/test_watershed.py
+++ tests/test_watershed.py
@@ -24,3 +24,38 @@ class WatershedUnitTests(VCRTestCase):
         assert '04150305' in str(wshed)
         assert str(wshed.lat) in str(wshed)
         assert str(wshed.lon) in str(wshed)
+
+    @staticmethod
+    def test_get_geojson():
+        """check a few random properties of the returned geojson dict"""
+        shed = Watershed(40.009631, -105.242433)
+        result = shed.get_geojson()
+        assert result['type'] == 'FeatureCollection'
+        assert result['features'][0]['properties']['I6H2Y'] == 1.263
+        assert result['features'][0]['properties']['LC11DEV'] == 100
+        assert result['features'][0]['bbox'][2] == -105.24254752674744
+        assert result['features'][0]['geometry']['coordinates'][0][00][1] \
+            == 40.006538265576346
+
+    @staticmethod
+    def test_get_geojson_raises_error():
+        """check that if the data is bad, we get an error"""
+        shed = Watershed(40.009631, -105.242433)
+        del shed.data['featurecollection'][1]  # delete the data we need
+        message = ''
+        try:
+            shed.get_geojson()
+        except LookupError as error:
+            message = str(error)
+
+        assert message == 'Could not find "globalwatershed" in the feature' \
+                          'collection.'
+
+    @staticmethod
+    def test_get_boundary():
+        """test the bounding box for a given case"""
+        shed = Watershed(40.009631, -105.242433)
+        result = shed.get_boundary()
+
+        assert result == [-105.2410132678883, 40.009688732073656,
+                          -105.24254752674744, 40.00653802187368]
