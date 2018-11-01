dc8009158ffe8934bec4b8e685b82b5e71540e80
Scott Eilerman <scott.eilerman@jupiterintel.com>
2018-11-01 11:55:08 -0600

    create test classes based on VCRUnitTest
    This allows automatic recording of "cassettes" whenever a test first requests data from a remote web endpoint. The cassettes are then "played back" from disk the next time that identical web request is made. This allows offline testing and prevents web service failures interfering with testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,24 +2,33 @@
 """Tests for streamstats utility functions."""
 
 import pytest
+from vcr_unittest import VCRTestCase
 from streamstats import utils
 
 
-def test_find_address():
-    """Verify that addresses are retrieved."""
-    address = utils.find_address(lat=40.0076, lon=-105.2659)
-    assert address['city'] == 'Boulder'
-    assert address['state'] == 'Colorado'
+class StreamStatsUtilTests(VCRTestCase):
+    '''
+    Using a test class based on VCRTestCase allows automatic capture of
+    web responses into "cassettes" upon the first run of a given test.
+    These cassettes will be "played back" on future tests.
+    '''
 
+    @staticmethod
+    def test_find_address():
+        """Verify that addresses are retrieved."""
+        address = utils.find_address(lat=40.0076, lon=-105.2659)
+        assert address['city'] == 'Boulder'
+        assert address['state'] == 'Colorado'
 
-def test_find_state():
-    """Verify that the correct state is returned."""
-    address = utils.find_address(lat=40.0076, lon=-105.2659)
-    state = utils.find_state(address)
-    assert state == 'CO'
+    @staticmethod
+    def test_find_state():
+        """Verify that the correct state is returned."""
+        address = utils.find_address(lat=40.0076, lon=-105.2659)
+        state = utils.find_state(address)
+        assert state == 'CO'
 
-
-def test_canada_raises_errors():
-    """Points outside the U.S. should raise errors."""
-    with pytest.raises(AssertionError):
-        utils.find_address(lat=45.5017, lon=-73.5673)
+    @staticmethod
+    def test_canada_raises_errors():
+        """Points outside the U.S. should raise errors."""
+        with pytest.raises(AssertionError):
+            utils.find_address(lat=45.5017, lon=-73.5673)
