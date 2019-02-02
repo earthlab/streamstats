d4e87eb48f4c9f5c62b40e6165d7f9eac3e10dd6
Max Joseph <maxwellbjoseph@gmail.com>
2019-02-02 09:27:27 -0700

    Use pytest-vcr (#54)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,42 +2,37 @@
 """Tests for streamstats utility functions."""
 
 import pytest
-from vcr_unittest import VCRTestCase
 from streamstats import utils
 
 
-class StreamStatsUtilTests(VCRTestCase):
-    '''
-    Using a test class based on VCRTestCase allows automatic capture of
-    web responses into "cassettes" upon the first run of a given test.
-    These cassettes will be "played back" on future tests.
-    '''
+@pytest.fixture
+@pytest.mark.vcr()
+def address():
+    """A fixture for the address of Boulder, CO."""
+    return utils.find_address(lat=40.0076, lon=-105.2659)
 
-    @staticmethod
-    def test_find_address():
-        """Verify that addresses are retrieved."""
-        address = utils.find_address(lat=40.0076, lon=-105.2659)
-        assert address['city'] == 'Boulder'
-        assert address['state'] == 'Colorado'
 
-    @staticmethod
-    def test_find_state():
-        """Verify that the correct state is returned."""
-        address = utils.find_address(lat=40.0076, lon=-105.2659)
-        state = utils.find_state(address)
-        assert state == 'CO'
+def test_find_address(address):
+    """Verify that addresses are retrieved."""
+    assert address['city'] == 'Boulder'
+    assert address['state'] == 'Colorado'
 
-    @staticmethod
-    def test_canada_raises_errors():
-        """Points outside the U.S. should raise errors."""
-        with pytest.raises(AssertionError):
-            utils.find_address(lat=45.5017, lon=-73.5673)
 
-    @staticmethod
-    def test_find_address_err_mess():
-        """Error message provides lat & lon in correct order."""
-        try:
-            utils.find_address(lat=40, lon=-200)
-        except ValueError as err:
-            message = str(err)
-        assert ("lat=40" in message) and ("lon=-200" in message)
+def test_find_state(address):
+    """Verify that the correct state is returned."""
+    state = utils.find_state(address)
+    assert state == 'CO'
+
+
+@pytest.mark.vcr()
+def test_canada_raises_errors():
+    """Points outside the U.S. should raise errors."""
+    with pytest.raises(AssertionError):
+        utils.find_address(lat=45.5017, lon=-73.5673)
+
+
+@pytest.mark.vcr()
+def test_find_address_err_mess():
+    """Error message provides coordinates."""
+    with pytest.raises(ValueError, match='lat=40'):
+        utils.find_address(lat=40, lon=-200)
--- tests/test_watershed.py
+++ tests/test_watershed.py
@@ -3,67 +3,54 @@
 
 import json
 import pytest
-from vcr_unittest import VCRTestCase
 import geojson
 from streamstats import Watershed
 
 
-class WatershedUnitTests(VCRTestCase):
-    """
-    Using a test class based on VCRTestCase allows automatic capture of
-    web responses into "cassettes" upon the first run of a given test.
-    These cassettes will be "played back" on future tests.
-    """
+@pytest.fixture
+@pytest.mark.vcr()
+def shed():
+    """A watershed object to use across tests."""
+    return Watershed(lat=43.939, lon=-74.524)
 
-    @staticmethod
-    def test_watershed():
-        """Verify that the JSON response contains expected keys."""
-        wshed = Watershed(lat=43.939, lon=-74.524)
-        keys = wshed.data.keys()
-        assert 'workspaceID' in keys
-        assert 'featurecollection' in keys
-        assert 'parameters' in keys
-        assert 'messages' in keys
-        assert '04150305' in str(wshed)
-        assert str(wshed.lat) in str(wshed)
-        assert str(wshed.lon) in str(wshed)
 
-    @staticmethod
-    def test_get_boundary():
-        """check a few random properties of the returned geojson dict"""
-        shed = Watershed(lat=43.939, lon=-74.524)
-        result = shed.get_boundary()
-        geojson_out = geojson.loads(json.dumps(result))
-        assert geojson_out.is_valid
+def test_watershed(shed):
+    """Verify that the JSON response contains expected keys."""
+    keys = shed.data.keys()
+    assert 'workspaceID' in keys
+    assert 'featurecollection' in keys
+    assert 'parameters' in keys
+    assert 'messages' in keys
+    assert '04150305' in str(shed)
+    assert str(shed.lat) in str(shed)
+    assert str(shed.lon) in str(shed)
 
-    @staticmethod
-    def test_characteristics():
-        """Check that expected dict keys exist."""
-        shed = Watershed(lat=43.939, lon=-74.524)
-        obs_keys = shed.characteristics().keys()
-        expected_keys = [p['code'] for p in shed.parameters]
-        for key in expected_keys:
-            assert key in obs_keys
 
-    @staticmethod
-    def test_get_characteristic():
-        """Get characteristic fails with no argument, succeeds w/ valid arg."""
-        shed = Watershed(lat=43.939, lon=-74.524)
-        with pytest.raises(ValueError):
-            shed.get_characteristic()
-        storage = shed.get_characteristic('STORAGE')
-        assert storage.get('name') == 'Percent Storage'
+def test_boundary(shed):
+    """check a few random properties of the returned geojson dict"""
+    result = shed.boundary
+    geojson_out = geojson.loads(json.dumps(result))
+    assert geojson_out.is_valid
 
-    @staticmethod
-    def test_get_boundary_raises_error():
-        """check that if the data is bad, we get an error"""
-        shed = Watershed(lat=43.939, lon=-74.524)
-        del shed.data['featurecollection'][1]  # delete the data we need
-        message = ''
-        try:
-            shed.get_boundary()
-        except LookupError as error:
-            message = str(error)
 
-        assert message == 'Could not find "globalwatershed" in the feature' \
-                          'collection.'
+def test_characteristics(shed):
+    """Check that expected dict keys exist."""
+    obs_keys = shed.characteristics.keys()
+    expected_keys = [p['code'] for p in shed.parameters]
+    for key in expected_keys:
+        assert key in obs_keys
+
+
+def test_get_characteristic(shed):
+    """Get characteristic fails with no argument, succeeds w/ valid arg."""
+    with pytest.raises(ValueError):
+        shed.get_characteristic()
+    storage = shed.get_characteristic('STORAGE')
+    assert storage.get('name') == 'Percent Storage'
+
+
+def test_boundary_raises_error(shed):
+    """check that if the data is bad, we get an error"""
+    del shed.data['featurecollection'][1]  # delete the data we need
+    with pytest.raises(LookupError, match='Could not find "globalwatershed"'):
+        shed.boundary
