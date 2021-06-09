8ec5f23d14040ca8d91f89cbc0f32277791269f6
aefitts <alfi9952@colorado.edu>
2021-06-09 10:32:41 -0600

    Revised vignettes to allign with comments and suggestions

--- examples/plot_create_plots.py
+++ examples/plot_create_plots.py
@@ -3,11 +3,9 @@ Download Data with StreamStats
 ===============================================================
 
 Learn how to use StreamStats python library to download watershed boundary data
-available in USGS StreamStats API. Watersheds in the United States can be
-defined using a coordinate reference system (latitude and longitude
-coordinates). StreamStats provides information including HUC code, a GeoJSON
-representation of the polygon associated with the watershed, and basin
-characteristics.
+available in USGS StreamStats API. StreamStats provides information including
+HUC code, a GeoJSON representation of the polygon associated with the watershed,
+and basin characteristics.
 """
 
 ###########################################################################
@@ -21,11 +19,11 @@ import streamstats
 import geopandas as gpd
 
 ###########################################################################
-# Indentify watershed
+# Identify watershed
 # ---------------------
-# Identify a spatial point using coordinate system (e.g., longitude and
-# latitude cordinates). Assign cordinates to variables ``lat`` and ``lon``.
-# Using StreamStat's data, assign location to a variable that will represent the
+# To identify a HUC, use a latitude and longitude value to select a specific watershed.
+# Assign cordinates to variables ``lat`` and ``lon``. Using StreamStat's data,
+# assign location to a variable that will represent the
 # delineated watershed using the USGS StreamStats API.
 
 lat, lon = 39.966256, -105.482227
@@ -34,9 +32,9 @@ ws = streamstats.Watershed(lat=lat, lon=lon)
 ###########################################################################
 # Find boundary properties of the watershed
 # ------------------------------------------
-# The ``ws.boundary()`` function will return the full watershed GeoJSON as a
-# dictionary. ESPG identifies the coordinate reference system (crs) used to
-# project the watershed.
+# ``ws.boundary`` is a stored variable in the watershed object and will return
+# the full watershed GeoJSON as a dictionary. You can access the CRS through
+# the GeoJSON object.
 
 ws.boundary
 ws.boundary['crs']
@@ -44,43 +42,10 @@ ws.boundary['crs']
 ###########################################################################
 # Create plot of the Watershed
 # -----------------------------
-# Using geopandas and the coordinate reference system, assign geodataframe to
-# variable and plot data.
+# Open the GeoJSON with GeoPandas and plot the data.
 
 ws.boundary
 poly = gpd.GeoDataFrame.from_features(ws.boundary["features"], crs="EPSG:4326")
-ax = poly.plot(figsize=(20, 10), alpha=0.5, edgecolor='k', legend=True)
+ax = poly.plot(figsize=(20, 10), edgecolor='k')
 ax.set_title("Single Watershed", fontsize=30, fontweight = 'bold')
 ax.set_axis_off()
-
-###########################################################################
-# Display multiple watersheds
-# -----------------------------
-# StreamStats allows users to plot multiple watersheds on a singular plot.
-# The example below plots the previously defined watershed and an additional
-# watershed on a single plot. Similar to plotting an individual watershed,
-# user must define additional watershed.
-
-# Assign coordinates to variables
-lat, lon = 39.869333, -105.749961
-ws2 = streamstats.Watershed(lat=lat, lon=lon)
-
-# Assign individual geodataframes to variables, assuring they have the same coordinate reference scale.
-poly1 = gpd.GeoDataFrame.from_features(
-    ws.boundary["features"], crs="EPSG:4326")
-poly2 = gpd.GeoDataFrame.from_features(
-    ws2.boundary["features"], crs="EPSG:4326")
-##############################################################################
-# Function ``ax`` allows users to plot two variables on the same graph.
-# The ``ax`` function also allows users to customize graph properties.
-# Attribute ``figsize`` adjusts plot size, ``alpha`` allows users to adjust
-# watershed transparency, ``edgecolor`` defines the watershed boundary color
-# (`list of colors <https://matplotlib.org/stable/gallery/color/named_colors.html>`_ ).
-# Visit this `tutorial <https://geopandas.org/docs/user_guide/mapping.html>`_.
-# to explore additional options for customizing maps and plots with geopandas.
-
-ax = poly1.plot(figsize=(20, 10), alpha=0.5, edgecolor='k', legend=True)
-poly2.plot(figsize=(20, 10), alpha=0.5, edgecolor='k', legend=True, ax=ax)
-ax.set_title("Multiple Watersheds", fontsize=30, fontweight='bold')
-ax.set_axis_off()
-###########################################################################
--- examples/plot_get_characteristics.py
+++ examples/plot_get_characteristics.py
@@ -16,9 +16,9 @@ import geopandas as gpd
 ###########################################################################
 # Indentify watershed
 # ---------------------
-# Identify a spatial point using coordinate system (e.g., longitude and latitude
-# cordinates). Assign cordinates to variables ``lat`` and ``lon``. Using
-# StreamStat's data, assign location to a variable that will represent the
+# To identify a HUC, use a latitude and longitude value to select a specific watershed.
+# Assign cordinates to variables ``lat`` and ``lon``. Using StreamStat's data,
+# assign location to a variable that will represent the
 # delineated watershed using the USGS StreamStats API.
 
 lat, lon = 39.966256, -105.482227
@@ -30,15 +30,16 @@ ws = streamstats.Watershed(lat=lat, lon=lon)
 # The USGS delineates watershed using a series of numbers based a hierarchal
 # region system. Every watershed is assigned a series of numbers called the
 # hydrological unit code (HUC). StreamStats uses HUC to identify and delineate
-# watersheds. The ``ws.huc`` function will return the HUC of the identified
-# watershed.
+# watersheds. `Learn more <https://water.usgs.gov/GIS/huc.html>`_ about
+# Hydrologic Units . The ``ws.huc`` function will return the HUC of the
+# identified watershed.
 
 ws.huc
 
 ###########################################################################
 # Find Characteristics of the watershed
 # --------------------------------------
-# The function ``ws.characteristics`` will return the available basin
+# The function ``ws.characteristics()`` will return the available basin
 # characteristics for the identified watershed.In order to return information
 # on a specific characteristic, use function
 # ``ws.get_characteristic('StatLabel')``. `Learn more <https://streamstatsags.cr.usgs.gov/ss_defs/basin_char_defs.aspx>`_
